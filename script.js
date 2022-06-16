702b7b4f7502d8628dfc90f075a49ab724a5c4fa
teacherIan <ian_malloy@ssis.asia>
2022-06-16 19:38:10 +0800

    visual updates

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const createGameEnv = () => {
     if (container.style.opacity < 1) {
       currentOpacity += 0.02;
       container.style.opacity = currentOpacity;
-      if (sabre.style.opacity < 0.15) {
+      if (sabre.style.opacity < 0.1) {
         sabre.style.opacity = currentOpacity;
       }
     } else {
